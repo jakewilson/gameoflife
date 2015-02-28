922089f5ee8bc62d7a62d03d4aad5d880159c3a8
Jake Wilson <jakewilsonfl@gmail.com>
2015-02-27 21:28:20 -0500

    Changes all tabs to 4 spaces for consistency. Thank you, Vim!

--- gol.js
+++ gol.js
@@ -1,21 +1,21 @@
-var     gridWidth  = 200,
-        gridHeight = 100,
-        cellWidth  = 5,
-        cellHeight = 5,
-        alivePercentage = 0,
-        canvas,
-        grid, // the grid
-        context, // the drawing context
-        running = false,
-        isMouseDown = false,
-        state = {
-            ALIVE: 0,
-            DEAD:  1
-        },
-        fillStyles = ['rgb(200, 200, 200)', 'rgb(0, 0, 0)'],
-        intervalID, // the id for the drawing interval
-        waitTime, // time in between draws in milliseconds
-        aliveCells = []; // list for holding all cells that will be alive on the next time step
+var gridWidth  = 200,
+    gridHeight = 100,
+    cellWidth  = 5,
+    cellHeight = 5,
+    alivePercentage = 0,
+    canvas,
+    grid, // the grid
+    context, // the drawing context
+    running = false,
+    isMouseDown = false,
+    state = {
+        ALIVE: 0,
+        DEAD:  1
+    },
+    fillStyles = ['rgb(200, 200, 200)', 'rgb(0, 0, 0)'],
+    intervalID, // the id for the drawing interval
+    waitTime, // time in between draws in milliseconds
+    aliveCells = []; // list for holding all cells that will be alive on the next time step
 
 var profileNames = ['Bomb', 'Tree'];
 var centerY = gridHeight / 2, centerX = gridWidth / 2;
@@ -27,74 +27,74 @@ var profiles = {
 };
 
 function Cell(x, y, alive) {
-        this.x = x;
-        this.y = y;
-        this.w = cellWidth;
-        this.h = cellHeight;
-        this.a = alive;
-        this.draw = function() {
-                context.fillStyle = fillStyles[this.a];
-                context.fillRect(this.x * this.w, this.y * this.h, this.w, this.h);
-        };
-        this.toggle = function() {
-            this.a = this.a === state.ALIVE ? state.DEAD : state.ALIVE;
-        }
+    this.x = x;
+    this.y = y;
+    this.w = cellWidth;
+    this.h = cellHeight;
+    this.a = alive;
+    this.draw = function() {
+            context.fillStyle = fillStyles[this.a];
+            context.fillRect(this.x * this.w, this.y * this.h, this.w, this.h);
+    };
+    this.toggle = function() {
+        this.a = this.a === state.ALIVE ? state.DEAD : state.ALIVE;
+    }
 }
 
 function Grid(w, h, ctx, url) {
-        this.count = 0;
-        this.w = w;
-        this.h = h;
-        this.ctx = ctx;
-        this.me = this;
-        this.init = function() {
-                this.grid = [];
-                for (var i = 0; i < this.h; i++) {
-                        this.grid[i] = [];
-                        for (var j = 0; j < this.w; j++) {
-                                this.grid[i][j] = new Cell(j, i, state.DEAD);
-                        }
-                }
-
-                // turn on a profile, if any
-                var regex = /[?](.+)/i;
-                if (regex.test(url)) {
-                    var profile = '';
-                    try  {
-                        profile = JSON.parse(decodeURIComponent(url.split(regex)[1]));
-                    } catch (e) {
-                        console.log('Invalid JSON.');
+    this.count = 0;
+    this.w = w;
+    this.h = h;
+    this.ctx = ctx;
+    this.me = this;
+    this.init = function() {
+            this.grid = [];
+            for (var i = 0; i < this.h; i++) {
+                    this.grid[i] = [];
+                    for (var j = 0; j < this.w; j++) {
+                            this.grid[i][j] = new Cell(j, i, state.DEAD);
                     }
-                    initProfile(profile);
-                }
-        };
+            }
 
-        this.draw = function() {
-                for (var i = 0; i < grid.h; i++) {
-                        for (var j = 0; j < grid.w; j++) {
-                                grid.grid[i][j].draw(ctx);
-                        }
+            // turn on a profile, if any
+            var regex = /[?](.+)/i;
+            if (regex.test(url)) {
+                var profile = '';
+                try  {
+                    profile = JSON.parse(decodeURIComponent(url.split(regex)[1]));
+                } catch (e) {
+                    console.log('Invalid JSON.');
                 }
-        };
+                initProfile(profile);
+            }
+    };
 
-        // moves the grid forward one time step
-        this.step = function() {
-                var nextGen = [], i, j;
-                for (i = 0; i < grid.h; i++) {
-                        nextGen[i] = [];
-                        for (j = 0; j < grid.w; j++) {
-                                if ((nextGen[i][j] = next(grid.grid[i][j]))) {
-                                        aliveCells.push(grid.grid[i][j]);
-                                }
-                        }
-                }
-                for (i = 0; i < grid.h; i++) {
-                        for (j = 0; j < grid.w; j++) {
-                            grid.grid[i][j].a = nextGen[i][j];
-                        }
-                }
-               grid.draw();
-        };
+    this.draw = function() {
+            for (var i = 0; i < grid.h; i++) {
+                    for (var j = 0; j < grid.w; j++) {
+                            grid.grid[i][j].draw(ctx);
+                    }
+            }
+    };
+
+    // moves the grid forward one time step
+    this.step = function() {
+            var nextGen = [], i, j;
+            for (i = 0; i < grid.h; i++) {
+                    nextGen[i] = [];
+                    for (j = 0; j < grid.w; j++) {
+                            if ((nextGen[i][j] = next(grid.grid[i][j]))) {
+                                    aliveCells.push(grid.grid[i][j]);
+                            }
+                    }
+            }
+            for (i = 0; i < grid.h; i++) {
+                    for (j = 0; j < grid.w; j++) {
+                        grid.grid[i][j].a = nextGen[i][j];
+                    }
+            }
+           grid.draw();
+    };
 
 }
 
@@ -102,19 +102,19 @@ function Grid(w, h, ctx, url) {
  * Returns the state of a cell in the next generation
  */
 function next(cell) {
-        var nbrs = 0;
-        for (var i = cell.y - 1; i <= cell.y + 1; i++) {
-                for (var j = cell.x - 1; j <= cell.x + 1; j++) {
-                       if (!outOfBounds(i, j)) { 
-                            nbrs += ((grid.grid[i][j].a === state.ALIVE) && (i != cell.y || j != cell.x)) ? 1 : 0;
-                        }
-                }
-        }
-        if (cell.a === state.ALIVE) {
-            return (nbrs === 2 || nbrs === 3) ? state.ALIVE : state.DEAD;
-        } else {
-            return (nbrs === 3) ? state.ALIVE : state.DEAD;
-        }
+    var nbrs = 0;
+    for (var i = cell.y - 1; i <= cell.y + 1; i++) {
+            for (var j = cell.x - 1; j <= cell.x + 1; j++) {
+                   if (!outOfBounds(i, j)) { 
+                        nbrs += ((grid.grid[i][j].a === state.ALIVE) && (i != cell.y || j != cell.x)) ? 1 : 0;
+                    }
+            }
+    }
+    if (cell.a === state.ALIVE) {
+        return (nbrs === 2 || nbrs === 3) ? state.ALIVE : state.DEAD;
+    } else {
+        return (nbrs === 3) ? state.ALIVE : state.DEAD;
+    }
 }
 
 /**
@@ -192,19 +192,19 @@ function selectProfile(){
 }
 
 function init() {
-        canvas = document.getElementById('canvas');
-        canvas.width  = gridWidth * cellWidth;
-        canvas.height = gridHeight * cellHeight;
-        canvas.addEventListener('mousedown', mousedown, false);
-        canvas.addEventListener('mouseup', mouseup, false);
-        canvas.addEventListener('mouseout', mouseup, false);
-        canvas.addEventListener('mousemove', mousemove, false);
-        context = canvas.getContext('2d');
-        addProfiles();
-        waitTime = 500;
-        grid = new Grid(gridWidth, gridHeight, context, document.URL);
-        grid.init();
-        grid.draw();
+    canvas = document.getElementById('canvas');
+    canvas.width  = gridWidth * cellWidth;
+    canvas.height = gridHeight * cellHeight;
+    canvas.addEventListener('mousedown', mousedown, false);
+    canvas.addEventListener('mouseup', mouseup, false);
+    canvas.addEventListener('mouseout', mouseup, false);
+    canvas.addEventListener('mousemove', mousemove, false);
+    context = canvas.getContext('2d');
+    addProfiles();
+    waitTime = 500;
+    grid = new Grid(gridWidth, gridHeight, context, document.URL);
+    grid.init();
+    grid.draw();
 }
 
 window.onload = function() {
